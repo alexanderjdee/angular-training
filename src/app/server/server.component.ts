52c69bf4cda78fb0b636884a91531b5210b23219
alexander.dee <alexander.dee@bisimulations.com>
2018-12-05 14:45:05 -0500

    Updated dependencies

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,9 +1,19 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
+  //Element selector
   selector: 'app-server',
-  templateUrl: ''
+
+  //Attribute selector
+  // selector: '[app-server]',
+
+  //Class selector
+  // selector: '.app-server'
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
+
+  constructor() { }
 
 }
